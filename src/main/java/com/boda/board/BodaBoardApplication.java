4acc6d29e05a1200044cee013937ae71c2abd90f
Jae <gjwodud119@gmail.com>
2022-02-21 02:26:56 +0900

    Initial commit

---
+++ src/main/java/com/boda/board/BodaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.boda.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodaBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boda/board/BodaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boda.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
